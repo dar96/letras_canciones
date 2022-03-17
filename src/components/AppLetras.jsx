45a853daa370f888ccd4c098abcabf36891f6534
dar96 <darielfriascastillo@gmail.com>
2022-03-17 13:08:29 +0100

    first commit

---
+++ src/components/AppLetras.jsx
@@ -0,0 +1,31 @@
+import Formulario from "./Formulario"
+import Alerta from "./Alerta"
+import Letra from "./Letra"
+import useLetras from "../hooks/useLetras"
+import Spinner from "./Spinner"
+
+
+const AppLetras = () => {
+
+  const {alerta, letra, cargando}= useLetras()
+
+ 
+
+  return (
+    <>
+      <header>Búsqueda de letras de canciones</header>
+
+      <Formulario/>
+
+
+      <main>
+        {alerta ? <Alerta>{alerta}</Alerta> :
+         letra? <Letra/> : 
+         cargando? <Spinner/>:
+         <p className="text-center">Busca letras de tus artistas favoritos</p>}
+      </main>
+    </>
+  )
+}
+
+export default AppLetras
